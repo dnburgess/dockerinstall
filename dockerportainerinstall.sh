7e4dc4a48665e57d26c4f4fc2f9a74717ef48fe3
David Burgess <dnburgess@users.noreply.github.com>
2023-12-17 09:45:35 -0700

    Create dockerportainerinstall.sh
    Root install method used to add Portainer install via Portainer

---
+++ dockerportainerinstall.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+IFS=$(printf '\n\t')
+
+# Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+printf '\nDocker installed successfully\n\n'
+
+printf 'Waiting for Docker to start...\n\n'
+sleep 5
+
+# Docker Compose
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
+printf '\nDocker Compose installed successfully\n\n'
+sudo docker-compose -v
+
+sleep 5
+
+# Portainer
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
